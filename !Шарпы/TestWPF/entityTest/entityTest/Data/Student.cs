9dc32c31e3a28ec664a4817e0e4add69ebe4f06e
Freakovv <misha.simanovich@mail.ru>
2025-03-22 11:44:46 +0300

    webka and photoshop

---
+++ !Шарпы/TestWPF/entityTest/entityTest/Data/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entityTest.Data
+{
+    internal sealed class Student
+    {
+        public int Id { get; set; }
+        [MaxLength(20)]
+        public string Username { get; set; }
+        
+        // SHA-256
+        [MaxLength(64)]
+        public string Password { get; set; }
+
+        // MaxLength by Gmail, Outlook and other
+        [MaxLength(254)] 
+        public string Email { get; set; }
+
+        //List<Group> Groups { get; set; }
+
+        Student() { }
+    }
+}
---
+++ !Шарпы/TestWPF/entityTest/entityTest/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace entityTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            inputEmail = new TextBox();
+            inputId = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            dataGridView1 = new DataGridView();
+            btnSave = new Button();
+            btnDelete = new Button();
+            btnCancel = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(99, 61);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(137, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(99, 99);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(137, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // inputEmail
+            // 
+            inputEmail.Location = new Point(99, 146);
+            inputEmail.Name = "inputEmail";
+            inputEmail.Size = new Size(137, 23);
+            inputEmail.TabIndex = 2;
+            // 
+            // inputId
+            // 
+            inputId.Location = new Point(99, 191);
+            inputId.Name = "inputId";
+            inputId.Size = new Size(137, 23);
+            inputId.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(30, 68);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 16);
+            label1.TabIndex = 4;
+            label1.Text = "Username";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(32, 106);
+            label2.Name = "label2";
+            label2.Size = new Size(57, 16);
+            label2.TabIndex = 5;
+            label2.Text = "Password";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(35, 149);
+            label3.Name = "label3";
+            label3.Size = new Size(35, 16);
+            label3.TabIndex = 6;
+            label3.Text = "Email";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(32, 198);
+            label4.Name = "label4";
+            label4.Size = new Size(17, 16);
+            label4.TabIndex = 7;
+            label4.Text = "id";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = Color.White;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(298, 19);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(490, 419);
+            dataGridView1.TabIndex = 8;
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new Point(43, 317);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(75, 23);
+            btnSave.TabIndex = 9;
+            btnSave.Text = "btnSave";
+            btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(124, 317);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(75, 23);
+            btnDelete.TabIndex = 10;
+            btnDelete.Text = "btnDelete";
+            btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            btnCancel.Location = new Point(205, 317);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(75, 23);
+            btnCancel.TabIndex = 11;
+            btnCancel.Text = "btnCancel";
+            btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 16F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Gray;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnCancel);
+            Controls.Add(btnDelete);
+            Controls.Add(btnSave);
+            Controls.Add(dataGridView1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(inputId);
+            Controls.Add(inputEmail);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox inputEmail;
+        private TextBox inputId;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private DataGridView dataGridView1;
+        private Button btnSave;
+        private Button btnDelete;
+        private Button btnCancel;
+    }
+}
